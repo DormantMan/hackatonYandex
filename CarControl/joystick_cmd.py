5c259d2ecf09dde92b4c01b312bfc2036eff8a7b
Georgy <georgy.komarov@mail.ru>
2018-05-15 11:52:08 +0300

    Create joystick_cmd.py

---
+++ CarControl/joystick_cmd.py
@@ -0,0 +1,43 @@
+import socket
+
+
+class Joystick:
+    def __init__(self):
+        self.state = '00'
+        self.speed = 1500
+        self.angle = 90
+
+    def inc_speed(self):
+        if self.speed != 1200:
+            self.speed -= 10
+
+    def dec_speed(self):
+        if self.speed != 1700:
+            self.speed += 10
+
+    def left(self):
+        if self.angle != 70:
+            self.angle -= 5
+
+    def right(self):
+        if self.angle != 110:
+            self.angle += 5
+
+    def stop(self):
+        self.angle = 90
+        self.speed = 1500
+        self.state = '00'
+
+    def send(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = ('172.24.1.1', 1080)
+        sock.connect(server_address)
+        try:
+            message = '{}/{}/{}'.format(self.state, self.speed, self.angle)
+            print(message)
+            sock.sendall(message.encode())
+        finally:
+            print('closing socket')
+            sock.close()
+            if self.state == '00':
+                exit(0)
